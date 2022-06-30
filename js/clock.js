d8fd3d5e4c7d0b84e2f7cf99e5467bbb8c80ae1c
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-07-01 00:33:45 +0900

    padStart로 두자리수로 맞추기 (0추가)

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,11 @@
 const clock = document.querySelector("#clock");
 
 function getClock() {
-  const date = new Date()
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 getClock();
 setInterval(getClock, 1000);
