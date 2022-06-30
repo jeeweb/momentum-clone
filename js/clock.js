10f19b4de1a22e705d6ae5a297145b269d9fcc35
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-06-30 23:57:38 +0900

    getting current time

---
+++ js/clock.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date()
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+getClock();
+setInterval(getClock, 1000);
