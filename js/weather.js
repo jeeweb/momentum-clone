02455305c017515ef6b3dcc50be595f47a1bbe48
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-01-29 01:27:19 +0900

    geolocation 스타일 수정

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@ const API_KEY = "51b0f806de3cf1824ff4c58d036c95ce";
 function onGeoOk(position) {
   let lat = position.coords.latitude;
   let lng = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units="metric"`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -13,7 +13,7 @@ function onGeoOk(position) {
       city.innerText = data.name;
       weatherIcon.src = `img/weather-icons/${data.weather[0].icon}.png`;
       weather.innerText = `${data.weather[0].main}`;
-      temp.innerText = ` ${data.main.temp}℃`;
+      temp.innerText = ` ${Math.round(data.main.temp)}℃`;
       console.log(data);
     });
 }
