5a6fc8aba4da748e4907273e2531bee3ae05f43f
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-01-28 11:35:00 +0900

    background 이미지 변경

--- js/background.js
+++ js/background.js
@@ -1,9 +1,16 @@
-const images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
+const body = document.querySelector("body");
+const images = [
+  "patterns1.png",
+  "patterns2.png",
+  "patterns3.png",
+  "patterns4.png",
+  "patterns6.png",
+  "patterns7.png",
+  "patterns8.png",
+  "patterns9.png",
+  "patterns10.png",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `/img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+body.style.backgroundImage = `url(img/background/${chosenImage})`;
