5d51508201cd264abd66aa0b400ff171373aec60
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-01-29 23:10:54 +0900

    background 이미지 추가, point color random

--- js/background.js
+++ js/background.js
@@ -9,8 +9,14 @@ const images = [
   "patterns8.png",
   "patterns9.png",
   "patterns10.png",
+  "patterns11.png",
+  "image12.jpg",
+  "image13.jpg",
 ];
+const colors = ["#da93c2", "#e64a19", "#388e3c", "#304ffe", "#fbc02d"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenColor = colors[Math.floor(Math.random() * colors.length)];
 
 body.style.backgroundImage = `url(img/background/${chosenImage})`;
+document.documentElement.style.setProperty("--point", chosenColor);
